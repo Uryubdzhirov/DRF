aeef21029c18358b547e1660537ac22995940baf
Uryubdzhirov <Uryubdzhirov@gmail.com>
2021-12-30 13:56:26 +0000

    lesson_1. Code style fix

--- todo/usersapp/models.py
+++ todo/usersapp/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-   first_name = models.CharField(max_length=64)
-   last_name = models.CharField(max_length=64)
-   birthday_year = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    birthday_year = models.PositiveIntegerField()
