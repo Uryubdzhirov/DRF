b8c092900799f6599d717260b78e16fc168f059c
Uryubdzhirov <Uryubdzhirov@gmail.com>
2022-01-20 15:01:02 +0000

    lesson_4. Views update/

--- todo/todoapp/views.py
+++ todo/todoapp/views.py
@@ -1,11 +1,50 @@
-from rest_framework.viewsets import ModelViewSet
+from django.shortcuts import get_object_or_404
+from rest_framework import mixins
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
+
+from todoapp.filters import ProjectFilter
+
+
 from .models import Project, ToDo
 from .serializers import ProjectSerializer, ToDoSerializer
+
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class ToDoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
+
+
+class ToDoDeleteView(APIView):
+    def delete(self, request, pk=None):
+        todo = get_object_or_404(ToDo, pk=pk)
+        todo.is_active = False
+        todo.save(update_fields=["is_active"])
+        return Response({"status": "OK"})
+
+
+class ToDoViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    ToDoDeleteView,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = ToDo.objects.all()
+    serializer_class = ToDoSerializer
+    filterset_fields = ["project"]
+    pagination_class = ToDoLimitOffsetPagination
+
 
 class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
     queryset = Project.objects.all()
-
-class ToDoViewSet(ModelViewSet):
-    serializer_class = ToDoSerializer
-    queryset = ToDo.objects.all()
+    filterset_class = ProjectFilter
+    pagination_class = ProjectLimitOffsetPagination
--- todo/usersapp/models.py
+++ todo/usersapp/models.py
@@ -1,7 +1,14 @@
+from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+# Create your models here.
 
-class User(models.Model):
+
+class User(AbstractUser):
+    uuid = models.UUIDField(primary_key=True, default=uuid4())
+    email = models.EmailField(unique=True, blank=False)
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
-    birthday_year = models.PositiveIntegerField()
+    birthday = models.DateField(blank=True, null=True)
+
