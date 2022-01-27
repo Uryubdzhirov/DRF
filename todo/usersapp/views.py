18f8d9d8dc08650806340c2953afd493f3b707d7
Uryubdzhirov <Uryubdzhirov@gmail.com>
2022-01-27 14:40:26 +0000

    lesson_6.

--- todo/usersapp/views.py
+++ todo/usersapp/views.py
@@ -1,9 +1,13 @@
 # from django.shortcuts import render
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
+from rest_framework.permissions import AllowAny
 
 from .models import User
 from .serializers import UserModelSerializer
+
+
+# Create your views here.
 
 
 class UserModelViewSet(mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.UpdateModelMixin, GenericViewSet):
